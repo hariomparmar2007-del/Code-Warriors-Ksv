cd8c5423d0d79003cb3af7c3ffe3ed51acd4ed0e
Hariom parmar <hariomparmar2007@gmail.com>
2026-06-06 14:58:00 +0530

    Delete vendorbridge_project directory

--- vendorbridge_project/vendorbridge_project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include # include અહિયાં ઉમેર્યું છે
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')), # આપણી એપ્લિકેશનની બધી લિંક્સ અહિયાં કનેક્ટ કરી દીધી
-]
